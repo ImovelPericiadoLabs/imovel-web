add62b501ddfcf081301a4a0636880bf9ba4d0b9
jeffnts <jf.nts10@gmail.com>
2025-11-13 20:12:32 -0300

    fix(api/index.ts): set default API URL to 'https://api.imovelpericiado.com/v1' for better fallback handling

--- src/constants/api/index.ts
+++ src/constants/api/index.ts
@@ -1,4 +1,4 @@
-export const url = process.env.NEXT_PUBLIC_API_URL
+export const url = process.env.NEXT_PUBLIC_API_URL || 'https://api.imovelpericiado.com/v1'
 
 export const endpoint = {
   addresses: '/places/autocomplete/',
