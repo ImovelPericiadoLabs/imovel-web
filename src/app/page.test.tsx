0fc182ed919481acabdb1b319bef3ad8291dab42
jeffnts <jf.nts10@gmail.com>
2025-11-18 21:55:45 -0300

    feat(page): implement redirect to '/consultar-imovel' in Home component to enhance navigation flow test(page.test): update test to verify redirect is called with the correct path to ensure proper functionality

--- src/app/page.test.tsx
+++ src/app/page.test.tsx
@@ -1,10 +1,15 @@
-import { render, screen } from '@testing-library/react'
-import { describe, it, expect } from 'vitest'
+import { render } from '@testing-library/react'
+import { describe, it, expect, vi } from 'vitest'
 import Home from './page'
+import { redirect } from 'next/navigation'
+
+vi.mock('next/navigation', () => ({
+  redirect: vi.fn(),
+}))
 
 describe('Home', () => {
-  it('should render the heading with text Home', () => {
+  it('should call redirect with the correct path', () => {
     render(<Home />)
-    expect(screen.getByRole('heading', { name: /home/i })).toBeInTheDocument()
+    expect(redirect).toHaveBeenCalledWith('/consultar-imovel')
   })
 })
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
+import { redirect } from 'next/navigation'
+
 export default function Home() {
-  return (
-    <h1>Home</h1>
-  );
+  redirect('/consultar-imovel')
+  return <h1>home</h1>
 }
