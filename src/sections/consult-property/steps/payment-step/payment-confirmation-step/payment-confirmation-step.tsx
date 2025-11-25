d72bf025ca150f7eb32a9dd7cdf5d788944b30d2
jeffnts <jf.nts10@gmail.com>
2025-11-24 23:49:57 -0300

    refactor(payment-confirmation-step.tsx): remove mock transaction data and simplify PixPaymentPage rendering for cleaner code and improved readability

--- src/sections/consult-property/steps/payment-step/payment-confirmation-step/payment-confirmation-step.tsx
+++ src/sections/consult-property/steps/payment-step/payment-confirmation-step/payment-confirmation-step.tsx
@@ -8,22 +8,9 @@ export function PaymentConfirmationStep() {
 
   const paymentMethod = watch('paymentMethod')
 
-  const mockTransactionData = {
-    pixCode:
-      '00020126580014BR.GOV.BCB.PIX0114+551199999999520400005303986540610.005802BR5925MOCK PAY5925SAO PAULO62070503***6304B60E',
-    amount: '67,56',
-    expirationTime: '10:45',
-  }
-
   switch (paymentMethod) {
     case 'pix':
-      return (
-        <PixPaymentPage
-          pixCode={mockTransactionData.pixCode}
-          amount={mockTransactionData.amount}
-          expirationTime={mockTransactionData.expirationTime}
-        />
-      )
+      return <PixPaymentPage />
 
     default:
       return null
