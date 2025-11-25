f4c6b4db46c28bff0d2a3bc301b90b5551cccfcc
jeffnts <jf.nts10@gmail.com>
2025-11-25 00:22:19 -0300

    feat(pix-payment-page.tsx): add setStep function to form context for better step management in payment process fix(pix-payment-page.tsx): pass setStep to BottomSheet onClose to ensure correct navigation in the payment flow

--- src/sections/consult-property/steps/payment-step/pix/pix-payment-page.tsx
+++ src/sections/consult-property/steps/payment-step/pix/pix-payment-page.tsx
@@ -1,4 +1,5 @@
 'use client'
+import type { FormContextWithSteps } from '@/sections/consult-property/types'
 import Image from 'next/image'
 import { useState, useEffect } from 'react'
 import { useMutation } from '@tanstack/react-query'
@@ -29,7 +30,7 @@ export function PixPaymentPage() {
     resolver: zodResolver(validations),
   })
 
-  const { getValues } = useFormContext()
+  const { getValues, setStep } = useFormContext() as FormContextWithSteps
 
   function clearServerError() {
     setServerError('')
@@ -153,7 +154,7 @@ export function PixPaymentPage() {
         </div>
       </div>
 
-      <BottomSheet isOpen={isOpenBottomSheet}>
+      <BottomSheet isOpen={isOpenBottomSheet} onClose={() => setStep(5)}>
         <div className="p-4 pb-12 max-h-[70vh] overflow-y-auto flex flex-col gap-3">
           <div className="flex flex-row gap-3 items-center">
             <div className="rounded-full bg-violet-50 size-14 flex items-center justify-center">
