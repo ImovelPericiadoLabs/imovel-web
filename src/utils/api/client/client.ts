5305ac87eb3b3dea3167b5349e5527f30093a4c0
João Emanuel <joaoemanueljanauba@gmail.com>
2025-12-04 23:56:25 -0300

    fix: add headers

--- src/utils/api/client/client.ts
+++ src/utils/api/client/client.ts
@@ -22,7 +22,8 @@ const api = {
 
   async post(url: string, rawBody: object) {
     const isFormData = rawBody instanceof FormData
-    const headers: Record<string, string> = {}
+    const headers: Record<string, string> = { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36' }
+
 
     if (!isFormData) {
       headers['Content-Type'] = 'application/json'
